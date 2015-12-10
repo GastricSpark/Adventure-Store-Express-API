e4b776609f1b140fba791dd42d81652086a1fcd5
Harry Whewell <harry.whewell@myfuturesbright.co.uk>
2015-12-10 16:22:07 +0000

    Adding weapon sequelize model

--- app/models/weapon.js
+++ app/models/weapon.js
@@ -2,3 +2,79 @@
  * app/models/weapon.js
  * Created by HWhewell on 10/12/2015.
  */
+
+// call the packages we need
+var sequelize = require('sequelize');
+
+//define model
+var Weapon = sequelize.define('weapons',{
+        ref: sequelize.STRING,
+        type: sequelize.STRING,
+        name: sequelize.STRING,
+        desc: sequelize.STRING,
+        damage: sequelize.STRING,
+        price: sequelize.INTEGER,
+        quantity: sequelize.INTEGER
+    },
+    {
+        instanceMethod: {
+            retrieveAll: function(onSuccess, onError){
+                Weapon.findAll({}, {raw: true})
+                    .success(onSuccess).error(onError);
+            },
+
+            retrieveByid: function(weapon_id, onSuccess, onError){
+                var id = weapon_id;
+                Weapon.find({where: {id: id}}, {raw: true})
+                    .success(onSuccess).error(onError);
+            },
+
+            add: function(onSuccess, onError){
+                var ref = this.ref;
+                var type = this.type;
+                var name = this.name;
+                var desc = this.desc;
+                var damage = this.damage;
+                var price = this.price;
+                var quantity = this.quantity;
+
+                Weapon.build({
+                    ref: ref,
+                    type: type,
+                    name: name,
+                    desc: desc,
+                    damage:damage,
+                    price: price,
+                    quantity: quantity})
+                    .save().success(onSuccess).error(onError);
+            },
+
+            updateById: function(weapon_id, onSuccess, onError) {
+                var id = weapon_id;
+                var ref = this.ref;
+                var type = this.type;
+                var name = this.name;
+                var desc = this.desc;
+                var damage = this.damage;
+                var price = this.price;
+                var quantity = this.quantity;
+
+                Weapon.update({ref: ref,
+                        type: type,
+                        name: name,
+                        desc: desc,
+                        damage: damage,
+                        price: price,
+                        quantity: quantity},
+                    {where:{id: id}})
+                    .success(onSuccess).error(onError);
+            },
+
+            removeById: function(weapon_id, onSuccess, onError){
+                var id = weapon_id;
+                Weapon.destroy({where:{id: id}})
+                    .success(onSuccess).error(onError);
+            }
+        }
+    }
+);
