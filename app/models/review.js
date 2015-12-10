bcb6af3ddd7a1abad7993c86aec104618811cd7c
Harry Whewell <harry.whewell@myfuturesbright.co.uk>
2015-12-10 15:29:22 +0000

    Adding review sequelize model

--- app/models/review.js
+++ app/models/review.js
@@ -2,3 +2,57 @@
  * app/models/review.js
  * Created by HWhewell on 10/12/2015.
  */
+
+// call the packages we need
+var sequelize = require('sequelize');
+
+// define model
+var Review = sequelize.define('reviews',{
+        product_ref: sequelize.STRING,
+        stars: sequelize.INTEGER,
+        body: sequelize.STRING,
+        author: sequelize.STRING
+    },
+    {
+        instanceMethod: {
+            retrieveAll: function(onSuccess, onError){
+                Review.findAll({}, {raw: true})
+                      .success(onSuccess).error(onError);
+            },
+
+            retrieveByid: function(review_id, onSuccess, onError){
+                var id = review_id;
+                Review.find({where: {id: id}}, {raw: true})
+                    .success(onSuccess).error(onError);
+            },
+
+            add: function(onSuccess, onError){
+                var product_ref = this.product_ref;
+                var stars = this.stars;
+                var body = this.body;
+                var author = this.author;
+
+                Review.build({product_ref: product_ref, stars: stars, body: body, author: author})
+                    .save().success(onSuccess).error(onError);
+            },
+
+            updateById: function(review_id, onSuccess, onError) {
+                var id = review_id;
+                var product_ref = this.product_ref;
+                var stars = this.stars;
+                var body = this.body;
+                var author = this.author;
+
+                Review.update({product_ref: product_ref, stars: stars, body: body, author: author},
+                    {where:{id: id}})
+                    .success(onSuccess).error(onError);
+            },
+
+            removeById: function(review_id, onSuccess, onError){
+                var id = review_id;
+                Review.destroy({where:{id: id}})
+                    .success(onSuccess).error(onError);
+            }
+        }
+    }
+);
