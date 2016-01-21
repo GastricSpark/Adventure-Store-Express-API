49d98d7e90af1b5fb60201af7efcf253bc9c4e59
Harry Whewell <harry.whewell@myfuturesbright.co.uk>
2016-01-21 16:44:31 +0000

    adding Authentication to COR's headers

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ router.use(function(req, res, next) {
     console.log('Request being made.');
 
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     next(); // make sure to go to the next routes and don't stop here
 });
